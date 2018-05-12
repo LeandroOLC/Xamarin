85bd62dee0315c28805e65974b5ab251ef441d63
leandroolc.ads <leandroolc.ads@hotmail.com>
2018-05-12 19:36:26 -0300

    App01

---
+++ App_ConsultaCep/App_ConsultaCEP/App_ConsultaCEP/App_ConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using App_ConsultaCEP.Servico;
+using App_ConsultaCEP.Servico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_ConsultaCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			BOTAO.Clicked += BuscarCEP;
+		}
+
+		public void BuscarCEP(Object sender, EventArgs args)
+		{
+			string cep = CEP.Text.Trim();
+
+			if (isValidCEP(cep))
+			{
+				try
+				{
+					Endereco end = ViaCepServico.BuscarEnderecoViaCep(cep);
+
+					if(end != null)
+					{
+						RESULTADO.Text = string.Format("Endereco: {2} de {3},{0},{1}", end.Localidade, end.Uf, end.Logradouro, end.Bairro);
+					}
+					else
+					{
+						DisplayAlert("ERRO ","Endereço não foi encontrado", "OK");
+					}
+
+				}
+				catch (Exception e )
+				{
+
+					DisplayAlert("ERRO CRÌTICO",e.Message,"OK");
+				}
+			}
+		}
+		private bool isValidCEP(string cep)
+		{
+			bool valido = true;
+
+			if (cep.Length != 8)
+			{
+				DisplayAlert("ERRO", "Cep inválido! O CEP deve conter 8 caracteres.", "OK");
+
+				valido = false;
+			}
+			int NovoCEP = 0;
+			if(!int.TryParse(cep, out NovoCEP))
+			{
+				DisplayAlert("ERRO", "CEP inválido! O CEP deve ser composto apenas po números.", "OK");
+
+				valido = false;
+			}
+
+			return valido;
+		}
+	}
+}
---
+++ App_ConsultaCep/App_ConsultaCEP/App_ConsultaCEP/App_ConsultaCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,28 @@
+﻿using App_ConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace App_ConsultaCEP.Servico
+{
+    public class ViaCepServico
+    {
+		private static string EnderecoURl = "https://viacep.com.br/ws/{0}/json/";
+
+		public static Endereco BuscarEnderecoViaCep(string cep)
+		{
+			string NovoEnderecoURL = string.Format(EnderecoURl, cep);
+
+			WebClient wc = new WebClient();
+			string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+			Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+			if (end.Cep == null) return null;
+			
+			return end;
+		}
+	}
+}
